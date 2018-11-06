e44eba5cf4e4d09fb057a1adf46a09ca41add207
elizabethnnguyen <truc.nguyen@sjsu.edu>
2018-11-05 23:43:11 -0800

    idk what i did; but mainly working on project 3 and getting started

---
+++ Project3/sketch.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+let vid_0;
+let vid_0_playing = false;
+let vid_1;
+let vid_1_playing = false;
+let documentActive = false; // we need to make sure they engage with page before it can auto play
+
+
+function setup() {
+  // dophin vid 1!
+  vid_0 = createVideo(['assets/ASMR.mp4', 'ASMR.webmhd.webm']);;
+  vid_0.size(700, 800);
+
+  vid_0.mouseOver(playVid_0);
+  vid_0.mouseOut(pauseVid_0);
+}
+
+function draw() {}
+
+function playVid_0() {
+  if (documentActive) {
+    vid_0.loop();
+    vid_0_playing = true;
+  }
+}
+
+function pauseVid_0() {
+  vid_0.pause();
+  vid_0_playing = false;
+}
+
+function playVid_1() {
+  if (documentActive) {
+    vid_1.loop();
+    vid_1_playing = true;
+  }
+}
+
+function pauseVid_1() {
+  vid_1.pause();
+  vid_1_playing = false;
+}
+
+function mousePressed() {
+  documentActive = true; // since they have clicked, now we can play
+}
