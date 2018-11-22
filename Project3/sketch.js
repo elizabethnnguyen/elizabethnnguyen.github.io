2e96449f2f6eec16cbe6894b5c57d66805609e08
elizabethnnguyen <truc.nguyen@sjsu.edu>
2018-11-21 16:03:36 -0800

    added reviews and description buttons

--- Project3/sketch.js
+++ Project3/sketch.js
@@ -38,3 +38,32 @@ function pauseVid_0() {
 function mousePressed() {
   documentActive = true; // since they have clicked, now we can play
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
