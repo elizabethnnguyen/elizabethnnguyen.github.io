802fabea58400a702aaed0c85059882a317fbaa2
elizabethnnguyen <truc.nguyen@sjsu.edu>
2018-11-21 16:15:59 -0800

    forced product description to automatically show when load page

--- Project3/sketch.js
+++ Project3/sketch.js
@@ -64,6 +64,3 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
