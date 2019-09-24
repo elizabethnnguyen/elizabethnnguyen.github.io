9533590867943b13e61d76eb3d19acee2034f2c6
elizabethnnguyen <truc.nguyen@sjsu.edu>
2019-09-24 09:39:32 -0700

    fixed the alignment of the net art code; added links to the video thumbnails

--- digitalworks.js
+++ digitalworks.js
@@ -87,3 +87,28 @@ $(window).resize(function{
     var mainContainerWidth=windowWidth-100; // For example
     $("#yourMainContainer").css({"width":mainContainerWidth+"px"});
 });
+
+
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
