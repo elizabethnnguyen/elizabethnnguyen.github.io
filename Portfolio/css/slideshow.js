c534fedaadad4a3eff47c376742751a957b1ab16
elizabethnnguyen <elizabethnguyen02@gmail.com>
2020-09-22 00:24:22 -0700

    update
    adjusted home to have all projects on home; added bip

--- Portfolio/css/slideshow.js
+++ Portfolio/css/slideshow.js
@@ -1,3 +1,4 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
 var slideIndex = 1;
  showSlides(slideIndex);
 
@@ -24,3 +25,4 @@ var slideIndex = 1;
    slides[slideIndex-1].style.display = "block";
    dots[slideIndex-1].className += " active";
  }
+// }
